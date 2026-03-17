9a75ae40f19c2c1acce45eabf8ab7d350477f123
kr366 <konstantin.roeder@kcl.ac.uk>
2026-03-17 15:55:03 +0000

    copying the pathsample basic outline with simplified srun setup

--- src/main.f90
+++ src/main.f90
@@ -9,6 +9,10 @@ program ParallelSetupTest
    integer :: nargs
    integer :: jobcounter, j
    character(len=10) :: jobstr
+   integer, allocatable :: pid(:)
+   character(len=100) :: fname
+   integer :: piddone, status, newjob
+   logical :: killed
    
    ! check number of arguments
    nargs = command_argument_count()
@@ -36,6 +40,8 @@ program ParallelSetupTest
    call get_hostname()
    call get_slurmversion()
    
+   allocate(pid(ntasks))
+
    write(*,*) "Number of tasks: ", ntasks
    write(*,*) "Hostname: ", hostname
    write(*,*) "Slurm version: ", slurmversion
@@ -46,20 +52,105 @@ program ParallelSetupTest
       do j=1,ntasks
          jobcounter = jobcounter + 1
          write(jobstr,'(i8)') jobcounter
-         call job_string(jobcounter,"submit"//trim(adjustl(jobstr))//".sh")
+         fname = "submit"//trim(adjustl(jobstr))//".sh"
+         call job_string(jobcounter,fname)
+         call fork_subproc(pid(j))
+         if (pid(j).eq.0) then
+            call submit_new_proc(fname,gput)
+         end if
       end do
    else
       do j=1,njobs
          jobcounter = jobcounter + 1
          write(jobstr,'(i8)') jobcounter
-         call job_string(jobcounter,"submit"//trim(adjustl(jobstr))//".sh")
+         fname = "submit"//trim(adjustl(jobstr))//".sh"
+         call job_string(jobcounter,fname)
+         call fork_subproc(pid(j))
+         if (pid(j).eq.0) then
+            call submit_new_proc(fname,gput)
+         end if
       end do
    end if
+
    
    do while (jobcounter.lt.njobs)
+      killed=.false.
+      call wait_subproc(piddone,status)
+      newjob = -1
+      if (piddone.gt.0) then
+         write(*,*) "PID ", piddone, " finished with exit status: ", status
+         do j=1,ntasks
+
+         end do
       jobcounter = jobcounter + 1
       write(jobstr,'(i8)') jobcounter
       call job_string(jobcounter,"submit"//trim(adjustl(jobstr))//".sh")
    end do
  
+
+
+   IF (PIDDONE.GT.0) THEN
+      IF (DEBUG) WRITE(MYUNIT,'(A,I8,A,I6)') ' parexec> PID ',PIDDONE,' has finished with exit status ',STATUS
+      DO J2=1,NTASKS
+         IF (PIDDONE.EQ.PID(J2)) THEN
+            IF (STATUS.NE.0) KILLED=.TRUE.
+            ! INCOMPLETE JOBS WOULD IDEALLY RETURN A NON-ZERO EXIT CODE 
+            NEWJOB = J2
+            IF (DEBUG) WRITE(MYUNIT, '(2(A,I8))') ' parexec> PID ',PIDDONE,' has finished on core ',J2
+         ENDIF
+      ENDDO
+   ELSE
+      CALL FLUSH(MYUNIT) ! the child process may duplicate output without this line!
+      WRITE(MYUNIT,'(A,I20)') ' parexec> WARNING - WAIT returned system error code ',-PIDDONE
+      ! Try calling wait again to see if this fixes things. 
+      CALL EXECUTE_COMMAND_LINE('sleep 1.0')
+      CALL WAIT_SUBR(PIDDONE,STATUS)
+      WRITE(MYUNIT,'(2(A,I8))') ' parexec> on calling wait again pid=',PIDDONE,' status=',STATUS
+      IF (PIDDONE.LE.0) WRITE(MYUNIT,'(2(A,I8))') ' parexec> WARNING *** continuing for non-positive process id'
+      IF (STATUS.NE.0) STOP
+   END IF
+   CALL FLUSH(MYUNIT) ! the child process may duplicate output without this line!
+
+   JOBCOUNTER = JOBCOUNTER + 1
+   !wait for a short period to not run out of source ports
+   CALL EXECUTE_COMMAND_LINE("sleep 1.0")
+   !create submission script (note that at this stage all relevant folders and files exist locally)
+   CALL CREATE_JOBSTR(CURRGEN,JOBCOUNTER,SUBFILE)
+   !flush output before we fork child process
+   CALL FLUSH(MYUNIT)
+   !now create child process
+   CALL FORK_SUBR(PID(NEWJOB))
+   IF (PID(NEWJOB).EQ.0) THEN
+      CALL SUBMITJOB(SUBFILE)
+   END IF
+ENDDO
+!need to wait for the remaining jobs
+!at this stage the previous loop should have all cores filled with the final jobs
+!this means there should be NTASKS jobs left to be collected
+DO J1=1,NTASKS
+   CALL FLUSH(MYUNIT)
+   CALL WAIT_SUBR(PIDDONE,STATUS)
+   !IF (DEBUG) WRITE(MYUNIT,'(A,5I8)') ' parexec> PIDDONE,STATUS,PID=',PIDDONE,STATUS,PID(1:NTASKS)
+   CALL FLUSH(MYUNIT) ! the child process may duplicate output without this line!
+   IF (PIDDONE.GT.0) THEN
+      DO J2=1,NTASKS
+         IF (PIDDONE.EQ.PID(J2)) THEN
+            IF (DEBUG) WRITE(MYUNIT, '(2(A,I8))') ' parexec> PID ',PIDDONE,' has finished on core ',J2
+         ENDIF
+      ENDDO
+   ELSE
+      CALL FLUSH(6) ! the child process may duplicate output without this line!
+      WRITE(MYUNIT,'(A,I20)') ' parexec> WARNING - WAIT returned system error code ',-PIDDONE
+      ! Try calling wait again to see if this fixes things. 
+      CALL EXECUTE_COMMAND_LINE('sleep 1.0')
+      CALL WAIT_SUBR(PIDDONE,STATUS)
+      WRITE(MYUNIT,'(2(A,I8))') ' parexec> on calling wait again pid=',PIDDONE,' status=',STATUS
+      IF (PIDDONE.LE.0) WRITE(MYUNIT,'(2(A,I8))') ' parexec> WARNING *** continuing for non-positive process id'
+      IF (STATUS.NE.0) STOP
+   END IF               
+ENDDO
+!remove all submission scripts from this generation
+CALL EXECUTE_COMMAND_LINE('rm submit_GEN_*.sh')
+END SUBROUTINE PARALLEL_MUT
+
 end program ParallelSetupTest
---
+++ src/utils_job_submission.f90
@@ -0,0 +1,75 @@
+module utils
+
+   !utilities for forking and waiting
+   interface
+      !> C-binding to forking new job for gfortran
+      function gfortran_fork() bind(C, name="fork")
+         use iso_c_binding, only: c_int
+            integer(c_int) :: gfortran_fork
+      end function gfortran_fork
+
+      !> C-binding for wait command in gfortran
+      function gfortran_wait(ExitStatus) bind(C, name="wait")
+         use iso_c_binding, only: c_int
+         integer(c_int) :: gfortran_wait
+         integer(c_int), intent(out) :: ExitStatus
+      end function gfortran_wait
+   end interface
+
+   !wrapper for execution
+   interface
+      function exec_wrapper(cmd) bind(C, name="exec_wrapper")
+         use iso_c_binding, only: c_char, c_int
+         implicit none
+         character(kind=c_char) :: cmd(*)
+         character(kind=c_int) :: exec_wrapper
+      end function exec_wrapper
+   end interface
+
+   contains
+
+      subroutine submit_new_proc(fname,gput)
+         implicit none
+         character(len=100), intent(in) :: fname
+         logical, intent(in) :: gput
+         character(len=100) :: basestr
+
+         if (gput) then
+            call exec_subr('srun -n1 -N1 --exclusive -l --gres=gpu:1 ' // TRIM(ADJUSTL(FNAME)))
+         else
+            call exec_subr('srun -n1 -N1 --exclusive -l ' // TRIM(ADJUSTL(FNAME)))
+         end if
+      end subroutine submit_new_proc
+
+      subroutine wait_subproc(pid,ExitStatus)
+         implicit none
+         integer,intent(inout) :: pid,ExitStatus
+         pid=gfortran_wait(ExitStatus)
+         ExitStatus=ishft(ExitStatus,-8)
+      end subroutine wait_subproc
+
+      subroutine getpid_subproc(pid)
+         implicit none
+         integer,intent(out) :: pid
+         integer :: getpid
+         pid=getpid()
+      end subroutine getpid_subproc
+
+      subroutine fork_subproc(pid) ! returns zero in the child process, PID of child in parent process
+         implicit none
+         integer, intent(inout) :: pid
+         pid=gfortran_fork()
+      end subroutine fork_subproc
+
+      subroutine exec_subr(cmd)
+         use iso_c_binding, only: c_null_char
+         implicit none
+         character(len=*), intent(in) :: cmd
+         integer :: stat
+         
+         stat = exec_wrapper(trim(adjustl(cmd))//c_null_char)
+
+         write(*,*) "execute> soemthing has gone wrong, iostat: ", stat
+         stop
+      end subroutine exec_subr
+end module utils
